fbcae332beacf9dd28d13b0ed73e738181d48e6f
Madotzz <rahmatrahmadi920@gmail.com>
2024-02-26 20:14:22 +0800

    Tugas soal nomor 1

---
+++ Tugas1.java
@@ -0,0 +1,21 @@
+package struktur_data;
+public class Tugas1 {
+    public static void main(String[] args) {
+//      Array 1D bertipe data primitif
+        byte[] data1 = {1,2,3,4,5};
+        
+        short[] data2 = {32,33,54,44,66};
+        
+        int[] data3 = {43,66,78,99,100};
+        
+        long[] data4 = {1000,3232,6645,7965};
+        
+        float[] data5 = {182.3f,233.3f,585.0f};
+        
+        double[] data6 = {128.3,193.4,854.4,670.7};
+        
+        char[] data7 = {'A','B','C','D','E'};
+        
+        boolean[] data8 = {true,false,false,true,true};
+    }
+}
